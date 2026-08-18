8f7924d58610fc278745dee656c795e602c0e041
rebeca-sarrea <rebeca_sarrea@estudante.sesisenai.org.br>
2026-08-18 09:33:52 -0300

    up - aaaaaa

--- database/index.sql
+++ database/index.sql
@@ -1,6 +1,12 @@
 CREATE DATABASE restaurante;
 USE restaurante;
 
+Create TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL UNIQUE,
+);
+
 CREATE TABLE pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
@@ -8,4 +14,6 @@ CREATE TABLE pratos (
     preco DECIMAL(10, 2) NOT NULL,
     categoria VARCHAR(100),
     usuario_id INT NOT NULL,
+
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
