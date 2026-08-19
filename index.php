e10bfce57409fdf60c234f1892a0f88418add01f
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-08-19 08:58:06 -0300

    up - criação da pasta css

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!$resultado) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pratos</title>
-    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
 
